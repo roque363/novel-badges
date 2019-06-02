f12216e0222ee7c7c0058eb8ef9bbc1eac0fa6f5
Roque Alarcon <roquealarcon363@gmail.com>
2019-06-02 18:15:14 -0500

    add props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 
 import Badge from './components/Badge';
+import nino_profile from './assets/nino_profile.jpg';
+import nino from './assets/nino.jpg';
 
 function App() {
   return (
     <div className="App">
-      <Badge />
+      <Badge
+        firstName="Nino" 
+        lastName="Nakano"
+        badgeImage={nino}
+        avatarImage={nino_profile}
+        mangaTitle="5Toubun no Hanayome"
+
+      />
     </div>
   );
 }
--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import './Badge.css'
 
-import nino_profile from '../assets/nino_profile.jpg';
-import nino from '../assets/nino.jpg';
-
 class Badge extends React.Component {
   render() {
+    const {
+      firstName,
+      lastName,
+      avatarImage,
+      badgeImage,
+      mangaTitle
+    } = this.props
     return (
       <div className="badge">
         <div className="badge_header">
-          <img className="logo" src={nino} alt="Badge"/>
+          <img className="logo" src={badgeImage} alt="Badge"/>
         </div>
         <div className="badge_section-name">
-          <img className="badge_avatar" src={nino_profile} alt="Badge"/>
-          <h2>Nino <br/> Nakano</h2>
+          <img className="badge_avatar" src={avatarImage} alt={firstName}/>
+          <h2>{firstName}<br/>{lastName}</h2>
         </div>
         <div className="badge_section-info">
           <div className="info-group">
             <h3 class="info-secondary-font">Japanese</h3>
             <div class="badge-font">
               <span lang="ja">
-                <ruby><rb>中</rb><rp>(</rp><rt>なか</rt><rp>)</rp></ruby>
-                <ruby><rb>野</rb><rp>(</rp><rt>の</rt><rp>)</rp></ruby>
+                <ruby>
+                  <rb>中</rb>
+                  <rt>なか</rt>
+                </ruby>
+                <ruby>
+                  <rb>野</rb>
+                  <rp>(</rp>
+                  <rt>の</rt>
+                  <rp>)</rp>
+                </ruby>
                 <ruby><rb>二</rb><rp>(</rp><rt>に</rt><rp>)</rp></ruby>
                 <ruby><rb>乃</rb><rp>(</rp><rt>の</rt><rp>)</rp></ruby>
               </span>
@@ -30,12 +42,12 @@ class Badge extends React.Component {
           <div className="info-group">
             <h3 class="info-secondary-font">Rōmaji</h3>
             <div class="badge-font">
-              Nino Nakano
+              {firstName} {lastName}
             </div>
           </div>
         </div>
         <div className="badge_footer">
-          #5Toubun no Hanayome
+          #{mangaTitle}
         </div>
       </div>
     );
