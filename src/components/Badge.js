8fa25cf7f1ff2de93dcd443d3a547f610e2b713d
Roque Alarcon <roquealarcon363@gmail.com>
2019-06-02 11:35:11 -0500

    add bootstrap

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './Badge.css'
 
 import logo from '../assets/wafle_1.jpg';
+import nino from '../assets/nino.jpg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <img className="logo" src={logo} alt="Logo de la Web"></img>
-        <h1>Roque <br/> Alarcon</h1>
+      <div className="badge">
+        <div className="badge_header">
+          <img className="logo" src={nino} alt="Badge"/>
+        </div>
+        <div className="badge_section-name">
+          <img className="badge_avatar" src={logo} alt="Badge"/>
+          <h2>Roque <br/> Alarcon</h2>
+        </div>
+        <div className="badge_section-info">
+          <p>@roque363</p>
+        </div>
+        <div className="badge_footer">
+        </div>
       </div>
     );
   }
