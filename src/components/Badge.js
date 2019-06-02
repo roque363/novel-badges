8f1f3e99d81a23761ca2e393cf980d514d9e473a
Roque Alarcon <roquealarcon363@gmail.com>
2019-06-02 12:00:21 -0500

    add style in badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Badge.css'
 
-import logo from '../assets/wafle_1.jpg';
+import nino_profile from '../assets/nino_profile.jpg';
 import nino from '../assets/nino.jpg';
 
 class Badge extends React.Component {
@@ -12,13 +12,30 @@ class Badge extends React.Component {
           <img className="logo" src={nino} alt="Badge"/>
         </div>
         <div className="badge_section-name">
-          <img className="badge_avatar" src={logo} alt="Badge"/>
-          <h2>Roque <br/> Alarcon</h2>
+          <img className="badge_avatar" src={nino_profile} alt="Badge"/>
+          <h2>Nino <br/> Nakano</h2>
         </div>
         <div className="badge_section-info">
-          <p>@roque363</p>
+          <div className="info-group">
+            <h3 class="info-secondary-font">Japanese</h3>
+            <div class="badge-font">
+              <span lang="ja">
+                <ruby><rb>中</rb><rp>(</rp><rt>なか</rt><rp>)</rp></ruby>
+                <ruby><rb>野</rb><rp>(</rp><rt>の</rt><rp>)</rp></ruby>
+                <ruby><rb>二</rb><rp>(</rp><rt>に</rt><rp>)</rp></ruby>
+                <ruby><rb>乃</rb><rp>(</rp><rt>の</rt><rp>)</rp></ruby>
+              </span>
+            </div>
+          </div>
+          <div className="info-group">
+            <h3 class="info-secondary-font">Rōmaji</h3>
+            <div class="badge-font">
+              Nino Nakano
+            </div>
+          </div>
         </div>
         <div className="badge_footer">
+          #5Toubun no Hanayome
         </div>
       </div>
     );
