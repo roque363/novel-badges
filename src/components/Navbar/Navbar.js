980743aa3d8d9674270b4e90a414bb299a25749a
Roque Alarcon <roquealarcon363@gmail.com>
2019-07-26 20:27:03 -0500

    add changes in the styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import './navbar.css';
 
-import logo_icon from '../../assets/logo_icon.png';
+import logo_icon from '../../assets/logo_text.png';
 
 class BadgeNew extends Component {
   render() {
     return (
-      <div>
-        <a href="/">
-          <img src={logo_icon} alt=""/>
-        </a>
+      <div className="nav-container">
+        <div className="navbar">
+          <a href="/">
+            <img src={logo_icon} alt=""/>
+          </a>
+        </div>
       </div>
     );
   }
