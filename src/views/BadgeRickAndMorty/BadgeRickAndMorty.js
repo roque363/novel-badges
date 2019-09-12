768749e766e067141781e3f633fb7b0263a8ad75
Roque Alarcon <roquealarcon363@gmail.com>
2019-09-12 01:57:19 -0500

    add infinite scroll

--- src/views/BadgeRickAndMorty/BadgeRickAndMorty.js
+++ src/views/BadgeRickAndMorty/BadgeRickAndMorty.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './badgeRickAndMorty.scss';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import BadgeHero from '../../components/BadgeHero/BadgeHero';
 import Loader from '../../components/Loader/Loader';
@@ -17,19 +18,35 @@ function CharacterCard(props) {
   );
 }
 
+function LoaderBottom() {
+  return (
+    <React.Fragment>
+      <h4 className="loader-text">Cargando...</h4>
+    </React.Fragment>
+  );
+}
+
 class BadgeRickAndMorty extends React.Component {
   state = {
     nextPage: 1,
-    loading: true,
+    loading: false,
+    hasMore: false,
     error: null,
     data: {
+      info: {},
       results: []
     }
   };
+  _isMounted = true;
 
   componentDidMount() {
+    this._isMounted = true;
     this.fetchCharacters();
   };
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
   fetchCharacters = async () => {
     this.setState({ loading: true, error: null});
@@ -37,42 +54,55 @@ class BadgeRickAndMorty extends React.Component {
       const response = await fetch(`https://rickandmortyapi.com/api/character?page=${this.state.nextPage}`);
       const data = await response.json();
 
-      this.setState({
-        loading: false,
-        data: {
-          info: data.info,
-          results: [].concat(this.state.data.results, data.results)
-        },
-        nextPage: this.state.nextPage + 1,
-      });
+      if (this._isMounted) {
+        this.setState({
+          loading: false,
+          data: {
+            info: data.info,
+            results: [].concat(this.state.data.results, data.results)
+          },
+          nextPage: this.state.nextPage + 1,
+        });
+  
+        if (this.state.data.info.next !== "") {
+          this.setState({ hasMore: true })
+        } else {
+          this.setState({ hasMore: false })
+        }
+
+      }
+
     } catch(error) {
       this.setState({ loading: false, error: error });
     }
   };
 
   render() {
-    if(this.state.error) {
+    if (this.state.error) {
       return `Error:${this.state.error.message}`;
     }
-    return(
+    return (
       <React.Fragment>
         <BadgeHero title="Rick y Morty"/>
         {/* Content starts */}
-        <div className="container">
-          <div className="row">
-            {this.state.data.results.map(character => (
-              <div className="col-6 col-md-4 col-lg-3" key={character.id}>
-                <CharacterCard character={character} />
-              </div>
-            ))}
+        <InfiniteScroll
+          dataLength={this.state.data.results.length}
+          next={this.fetchCharacters}
+          hasMore={this.state.hasMore}
+          loader={<LoaderBottom/>}>
+          <div className="container">
+            <div className="row">
+              {this.state.data.results.map(character => (
+                <div className="col-6 col-md-4 col-lg-3" key={character.id}>
+                  <CharacterCard character={character} />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        </InfiniteScroll>
 
         {this.state.loading && ( <Loader/> )}
-
-        {!this.state.loading && (
-          <button onClick={() => this.fetchCharacters()}>Mostrar mas</button>
-        )}
+        {/* {!this.state.loading && ( <button onClick={() => this.fetchCharacters()}>Mostrar mas</button> )} */}
 
         {/* Content ends */}
       </React.Fragment>
