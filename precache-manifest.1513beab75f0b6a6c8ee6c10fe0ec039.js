f525968b0f347c1e0b68740de69a5f09042590e4
Roque Alarcon <roquealarcon363@gmail.com>
2019-09-13 14:59:22 -0500

    Updates

---
+++ precache-manifest.1513beab75f0b6a6c8ee6c10fe0ec039.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07f95c057a06bfee0fcece7c68c649e",
+    "url": "/novel-badges/index.html"
+  },
+  {
+    "revision": "6d5bb879e29573482795",
+    "url": "/novel-badges/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0ad2426b2a2bc5599c92",
+    "url": "/novel-badges/static/css/main.0de6f7f4.chunk.css"
+  },
+  {
+    "revision": "6d5bb879e29573482795",
+    "url": "/novel-badges/static/js/2.739a172b.chunk.js"
+  },
+  {
+    "revision": "0ad2426b2a2bc5599c92",
+    "url": "/novel-badges/static/js/main.8fd8b446.chunk.js"
+  },
+  {
+    "revision": "b599b507d6a65070969a",
+    "url": "/novel-badges/static/js/runtime~main.cc0064cb.js"
+  },
+  {
+    "revision": "7abeb9881c585c1806ce8433f1176a3c",
+    "url": "/novel-badges/static/media/arrow-next.7abeb988.svg"
+  },
+  {
+    "revision": "64226eee11972e42b6116f7abc1b7cbc",
+    "url": "/novel-badges/static/media/arrow-prev.64226eee.svg"
+  },
+  {
+    "revision": "75ea5578f4ed1c3d1b94b5b3758c8107",
+    "url": "/novel-badges/static/media/banner_1.75ea5578.jpg"
+  },
+  {
+    "revision": "0df83e363492e8a225fa69f688392656",
+    "url": "/novel-badges/static/media/banner_2.0df83e36.png"
+  },
+  {
+    "revision": "b74e7b9c40c23328a4f7c127255cc6d0",
+    "url": "/novel-badges/static/media/banner_3.b74e7b9c.jpeg"
+  },
+  {
+    "revision": "d185187feba0e4daa627f344d07cfad7",
+    "url": "/novel-badges/static/media/error.d185187f.gif"
+  },
+  {
+    "revision": "91f04904ccd5bf6eec530c0e44999edd",
+    "url": "/novel-badges/static/media/nino.91f04904.jpg"
+  },
+  {
+    "revision": "740357b014efff0797ee2645e519e720",
+    "url": "/novel-badges/static/media/nino_profile.740357b0.jpg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/novel-badges/static/media/stars.1d17360a.svg"
+  }
+]);
