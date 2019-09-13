1a563e9d6e03aa2de4f797951be0b0006a26ff98
Roque Alarcon <roquealarcon363@gmail.com>
2019-09-13 14:05:53 -0500

    production

---
+++ build/precache-manifest.6dfb6e6d506b873627233a40d230dd62.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0914652bfdd991a98e9ec525216dc149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5bb879e29573482795",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "08c3af090b5cf15c4c30",
+    "url": "/static/css/main.93c8f1e8.chunk.css"
+  },
+  {
+    "revision": "6d5bb879e29573482795",
+    "url": "/static/js/2.739a172b.chunk.js"
+  },
+  {
+    "revision": "08c3af090b5cf15c4c30",
+    "url": "/static/js/main.7ce89d27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7abeb9881c585c1806ce8433f1176a3c",
+    "url": "/static/media/arrow-next.7abeb988.svg"
+  },
+  {
+    "revision": "64226eee11972e42b6116f7abc1b7cbc",
+    "url": "/static/media/arrow-prev.64226eee.svg"
+  },
+  {
+    "revision": "75ea5578f4ed1c3d1b94b5b3758c8107",
+    "url": "/static/media/banner_1.75ea5578.jpg"
+  },
+  {
+    "revision": "0df83e363492e8a225fa69f688392656",
+    "url": "/static/media/banner_2.0df83e36.png"
+  },
+  {
+    "revision": "b74e7b9c40c23328a4f7c127255cc6d0",
+    "url": "/static/media/banner_3.b74e7b9c.jpeg"
+  },
+  {
+    "revision": "d185187feba0e4daa627f344d07cfad7",
+    "url": "/static/media/error.d185187f.gif"
+  },
+  {
+    "revision": "91f04904ccd5bf6eec530c0e44999edd",
+    "url": "/static/media/nino.91f04904.jpg"
+  },
+  {
+    "revision": "740357b014efff0797ee2645e519e720",
+    "url": "/static/media/nino_profile.740357b0.jpg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  }
+]);
