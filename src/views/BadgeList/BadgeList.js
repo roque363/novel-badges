9511edbc5c57303731943859ac14bb9c6fde0c27
Roque Alarcon <roquealarcon363@gmail.com>
2020-04-28 18:53:42 -0500

    add prettier file

--- src/views/BadgeList/BadgeList.js
+++ src/views/BadgeList/BadgeList.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import './badgeList.scss'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './badgeList.scss';
 import db from 'data.json';
 // Constans
-import * as ROUTES from 'constants/routes'
+import * as ROUTES from 'constants/routes';
 // Hooks
 import { useSearchSerie } from 'hooks/useSearchData';
 // Components
@@ -11,40 +11,38 @@ import BadgeHero from 'components/BadgeHero';
 import Loader from 'components/Loader';
 import SerieItem from 'components/SerieItem';
 
-const BadgeList = props => {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+const BadgeList = (props) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   const { query, setQuery, filteredData } = useSearchSerie(data);
 
   const fetchData = async () => {
     try {
-      const data = db
+      const data = db;
       setTimeout(() => {
-        setData(data.series)
-        setLoading(false)
+        setData(data.series);
+        setLoading(false);
       }, 800);
     } catch (error) {
-      setError(error.message)
-      setLoading(false)
+      setError(error.message);
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchData()
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   if (error) {
-    return <h3>{error}</h3>
+    return <h3>{error}</h3>;
   }
 
   return (
     <div className="badge-list">
-      <BadgeHero
-        title="Lista"
-      />
+      <BadgeHero title="Lista" />
       {!loading ? (
         <div className="badge-list__container">
           <div className="badge-list__container--buttons">
@@ -60,7 +58,7 @@ const BadgeList = props => {
                 type="text"
                 value={query}
                 onChange={(e) => {
-                  setQuery(e.target.value)
+                  setQuery(e.target.value);
                 }}
               />
             </div>
@@ -72,21 +70,21 @@ const BadgeList = props => {
                     <li key={serie.id}>
                       <SerieItem serie={serie} />
                     </li>
-                  )
+                  );
                 })}
               </ul>
             ) : (
-                <div>
-                  <h3>No se encontro ninguna serie</h3>
-                </div>
-              )}
+              <div>
+                <h3>No se encontro ninguna serie</h3>
+              </div>
+            )}
           </div>
         </div>
       ) : (
-          <Loader />
-        )}
+        <Loader />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default BadgeList;
