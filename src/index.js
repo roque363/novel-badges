87b5f9c9080f6aaa612da00677a78dfc85c1862c
Roque Alarcon <roquealarcon363@gmail.com>
2021-06-06 02:06:49 -0500

    :package: Remove unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// eslint-disable-next-line
-import $ from 'jquery';
-// eslint-disable-next-line
-import Popper from 'popper.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
